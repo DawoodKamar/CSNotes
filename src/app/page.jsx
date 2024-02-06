f8d11e6ed11b082c752613de976cdf5c424e0260
DawoodKamar <114121464+DawoodKamar@users.noreply.github.com>
2024-02-06 08:00:38 +0100

    test home page fetching with test json api

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,23 +2,36 @@
 import styles from "./home.module.css";
 import Postcard from "@/components/postcard/Postcard";
 
-export default function Home() {
-  return (
+const getData = async () =>{
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {next:{revalidate:3600}}) 
+  //it revalidates the data every hour
 
-      <div className={styles.container}>
-        <div className={styles.post}>
-          <Postcard />
-        </div>
-        <div className={styles.post}>
-          <Postcard />
-        </div>
-        <div className={styles.post}>
-          <Postcard />
-        </div>
-        <div className={styles.post}>
-          <Postcard />
-        </div>
-      </div>
-    
-  );
+  if (!res.ok){
+    throw new Error ("Something went wrong")
+  }
+
+  return res.json()
 }
+
+
+
+const Home = async () => {
+
+  const posts = await getData()
+
+  return (
+    <div className={styles.container}>
+      {posts.map((post)=>
+    <div className={styles.post} key={post.id}>
+      <Postcard post={post}/>
+    </div>
+ 
+      )}
+  </div>
+
+  )
+}
+
+export default Home
+
+
--- src/components/postcard/Postcard.jsx
+++ src/components/postcard/Postcard.jsx
@@ -1,16 +1,16 @@
 import Link from "next/link"
 import styles from "./postcard.module.css"
 
-const Postcard = () => {
+const Postcard = ({post}) => {
   return (
     <div className={styles.container}>
       <Link href="/blog/post">
         <div className="top">
-          <h1 className={styles.title}>title title title title title</h1>
+          <h1 className={styles.title}>{post.title}</h1>
           <p className={styles.date}>nov, 1,2022</p>
         </div> 
         <div className={styles.bottom}>
-          <p className={styles.desc}>this is the post discription this is the post discription this is the post discription this is the post discription this is the post discription this is the post discription this is the post discription this is the post discription this is the post discription this is the post discription </p>
+          <p className={styles.desc}>{post.body}</p>
         </div>
         <p className={styles.read}>Read more</p>
       </Link>
