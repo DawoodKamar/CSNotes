cb2d59b6a41ca6526a43ab806f7dabadd0f63dbc
DawoodKamar <114121464+DawoodKamar@users.noreply.github.com>
2024-02-14 22:02:56 +0100

    updated login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -2,9 +2,9 @@ import Styles from "./login.module.css"
 const LoginPage = () => {
   return (
     <div className={Styles.container}>
-      
       <div className={Styles.wrapper}>
-        login
+        <h3>Admin login</h3>
+        <div className={Styles.google}>Login with Google</div>
       </div>
       
     </div>
