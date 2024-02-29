da530ae6e5aa023e630d7024b1bf06e7d309227c
DawoodKamar <114121464+DawoodKamar@users.noreply.github.com>
2024-02-29 13:58:20 +0100

    fix pagination in topic page bug

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,16 +1,22 @@
 "use client"
 
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import styles from "./pagination.module.css";
 
 
 
 const Pagination = ({page, hasPrev, hasNext}) => {
   const router = useRouter()
+  const query = useSearchParams()
+  const topic = query.get("topic")
+  const next = `?page=${page+1}`
+  const prev = `?page=${page-1}`
+  
+  
   return (
     <div className={styles.container}>
-        <button className={styles.button} disabled={!hasPrev} onClick={()=>router.push(`?page=${page-1}`)}>Previous</button>
-        <button className={styles.button} disabled={!hasNext} onClick={()=>router.push(`?page=${page+1}`)}>Next</button>
+        <button className={styles.button} disabled={!hasPrev} onClick={()=>router.push(topic?`${prev}&topic=${topic}`:prev)}>Previous</button>
+        <button className={styles.button} disabled={!hasNext} onClick={()=>router.push(topic?`${next}&topic=${topic}`:next)}>Next</button>
     </div>
   )
 }
